ff997f9b8da83cfb781fdf315cbaf20dfd675963
griffinmacias <griffinmacias@gmail.com>
2015-07-02 14:42:56 -0400

    Works, didnt add the cocoa pods

--- theFinalCountdown/FISViewController.m
+++ theFinalCountdown/FISViewController.m
@@ -9,7 +9,12 @@
 #import "FISViewController.h"
 
 @interface FISViewController ()
-
+@property (weak, nonatomic) IBOutlet UIDatePicker *timePicker;
+@property (weak, nonatomic) IBOutlet UILabel *timerLabel;
+@property (nonatomic) NSInteger numberOfSeconds;
+@property (nonatomic, strong) NSTimer *timer;
+@property (weak, nonatomic) IBOutlet UIButton *pauseButton;
+@property (nonatomic, strong) NSDateComponents *timeComponents;
 @end
 
 @implementation FISViewController
@@ -17,8 +22,9 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
-
+    
+    
+    
 
 
 	// Do any additional setup after loading the view, typically from a nib.
@@ -26,15 +32,86 @@
 
 - (void)viewWillAppear:(BOOL)animated
 {
-
+    
+[self.timerLabel setHidden:YES];
 
 }
 
 
-- (void)didReceiveMemoryWarning
+- (IBAction)startButtonTapped:(UIButton *)sender {
+  
+    
+    self.pauseButton.enabled = YES;
+    
+    self.numberOfSeconds = self.timePicker.countDownDuration;
+    
+    self.timerLabel.text = [self changesSecondsIntoTheRightFormat];
+    
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:1.0
+                                     target:self
+                                                selector:@selector(startCountDown)
+                                   userInfo:nil
+                                    repeats:YES];
+    
+    
+    [self.timePicker setHidden:YES];
+    [self.timerLabel setHidden:NO];
+    
+    
+    if ([sender.titleLabel.text isEqualToString:@"Start"]) {
+        
+        [sender setTitle:@"Cancel" forState:UIControlStateNormal];
+        
+    } else {
+        
+        [self.timer invalidate];
+        [self.timerLabel setHidden:YES];
+        [self.timePicker setHidden:NO];
+        [self.pauseButton setEnabled:NO];
+        [sender setTitle:@"Start" forState:UIControlStateNormal];
+    
+    }
+}
+
+-(NSString *)changesSecondsIntoTheRightFormat
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    NSInteger seconds = self.numberOfSeconds % 60;
+    NSInteger minutes = (self.numberOfSeconds / 60) % 60;
+    NSInteger hours = self.numberOfSeconds / 3600;
+    
+   return [NSString stringWithFormat:@"%02li:%02li:%02li",hours, minutes, seconds];
+}
+
+-(void)startCountDown
+
+{
+    
+    self.numberOfSeconds--;
+    
+    if (self.numberOfSeconds <= 0) {
+        [self.timer invalidate];
+    } else {
+        
+        self.timerLabel.text = [self changesSecondsIntoTheRightFormat];
+    }
+}
+
+- (IBAction)pauseButtonTapped:(UIButton *)sender {
+    
+    if ([sender.titleLabel.text isEqualToString:@"Pause"]) {
+        [sender setTitle:@"Resume" forState:UIControlStateNormal];
+         [self.timer invalidate];
+        
+        
+    } else {
+        [sender setTitle:@"Pause" forState:UIControlStateNormal];
+        self.timer = [NSTimer scheduledTimerWithTimeInterval:1.0
+                                                      target:self
+                                                    selector:@selector(startCountDown)
+                                                    userInfo:nil
+                                                     repeats:YES];
+    }
+   
 }
 
 @end
